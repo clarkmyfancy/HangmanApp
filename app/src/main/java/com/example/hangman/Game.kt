3adcb9ff595a0f6e61b716741902c22c5b99e115
Jack Conrad Clark <clark.jack0@gmail.com>
2019-08-17 01:39:59 -0500

    removed unused class

--- app/src/main/java/com/example/hangman/Game.kt
+++
@@ -1,7 +0,0 @@
-package com.example.hangman
-
-class Game {
-    var secretWord: String = ""
-    var disguisedWord: String = ""
-    var guessedLetters: MutableList<Char> = mutableListOf<Char>()
-}
