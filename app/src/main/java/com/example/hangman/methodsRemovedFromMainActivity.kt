6d079878af5d53881c5a8e268f3a85121bccb746
Jack Conrad Clark <clark.jack0@gmail.com>
2019-08-17 00:12:28 -0500

    arbitrary file to contain will-be-deleted code

---
+++ app/src/main/java/com/example/hangman/methodsRemovedFromMainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.hangman
+
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AlertDialog
+
+//override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+//    super.onCreateOptionsMenu(menu)
+//    menuInflater.inflate(R.menu.menu, menu)
+//    return true
+//}
+//
+//override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//    if (item.itemId == R.id.action_about) {
+//        showInfo()
+//    }
+//    return true
+//}
+//
+//private fun showInfo() {
+////        val dialogTitle = getString(R.string.about_title, BuildConfig.VERSION_NAME)
+//    val dialogMessage = getString(R.string.about_message)
+//    val builder = AlertDialog.Builder(this)
+////        builder.setTitle(dialogTitle)
+//    builder.setMessage(dialogMessage)
+//    builder.create().show()
+//}
