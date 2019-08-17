3ae8f35a06c8cc249e4b4ddfd0fff488f5429503
Jack Conrad Clark <clark.jack0@gmail.com>
2019-08-17 18:02:24 -0500

    removed a lot of blank space

--- app/src/main/java/com/example/hangman/GuessWord/GuessWordFragment.kt
+++ app/src/main/java/com/example/hangman/GuessWord/GuessWordFragment.kt
@@ -1,18 +1,15 @@
 package com.example.hangman.GuessWord
 
-import android.database.DatabaseUtils
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import java.lang.StringBuilder
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.hangman.R
 import com.example.hangman.databinding.FragmentGuessWordBinding
 
-class GuessWord : Fragment() {
+class GuessWordFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
@@ -23,23 +20,7 @@ class GuessWord : Fragment() {
             false)
 
         return binding.root
-
     }
-
-
-//    private lateinit var binding: ActivityGuessWordBinding
-
-//    private var game = Game()
-
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//
-//        binding = DataBindingUtil.setContentView(this, R.layout.fragment_guess_word)
-//
-
-//    }
-
-
 }
 
 
