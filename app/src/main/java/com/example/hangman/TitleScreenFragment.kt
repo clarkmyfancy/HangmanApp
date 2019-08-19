37e382f84bf3be9321a4c8874c4e770591047b0b
Jack Conrad Clark <clark.jack0@gmail.com>
2019-08-19 08:40:05 -0500

    added log to playbutton onclick listener, for debug purposes

--- app/src/main/java/com/example/hangman/TitleScreenFragment.kt
+++ app/src/main/java/com/example/hangman/TitleScreenFragment.kt
@@ -1,11 +1,14 @@
 package com.example.hangman
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.example.hangman.R
 import com.example.hangman.databinding.FragmentTitleScreenBinding
 
@@ -19,6 +22,12 @@ class TitleScreenFragment : Fragment() {
             container,
             false
         )
+
+        binding.playGameButton.setOnClickListener {
+            Log.i("TitleScreenFragment", "onClickListener setup for startButton")
+            val action = TitleScreenFragmentDirections.actionTitleScreenFragmentToHowManyPlayersFragment()
+            findNavController().navigate(action)
+        }
 
         return binding.root
     }
