a8766a5e9f9aaefefdc938df6f72d7f5d629b1c6
clarkmyfancy <clark.jack0@gmail.com>
2019-08-21 14:17:10 -0500

    refactored and removed clutter

--- app/src/main/java/com/example/hangman/TitleScreen.kt
+++ app/src/main/java/com/example/hangman/TitleScreen.kt
@@ -1,18 +1,15 @@
 package com.example.hangman
 
 import android.os.Bundle
-import android.util.Log
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
-import com.example.hangman.R
+import androidx.navigation.Navigation
 import com.example.hangman.databinding.FragmentTitleScreenBinding
 
-class TitleScreenFragment : Fragment() {
+class TitleScreen : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
@@ -23,11 +20,9 @@ class TitleScreenFragment : Fragment() {
             false
         )
 
-        binding.playGameButton.setOnClickListener {
-            Log.i("TitleScreenFragment", "onClickListener setup for startButton")
-            val action = TitleScreenFragmentDirections.actionTitleScreenFragmentToHowManyPlayersFragment()
-            findNavController().navigate(action)
-        }
+        binding.startButton.setOnClickListener (
+            Navigation.createNavigateOnClickListener(R.id.action_titleScreen_to_selectGameMode)
+        )
 
         return binding.root
     }
