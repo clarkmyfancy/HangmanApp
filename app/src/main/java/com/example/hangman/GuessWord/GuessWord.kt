aaa6e600cda6a9bdd175fe5576c10d0d14a4a721
clarkmyfancy <clark.jack0@gmail.com>
2019-08-22 11:25:03 -0500

    beginning to implement MVVM

--- app/src/main/java/com/example/hangman/GuessWord/GuessWord.kt
+++ app/src/main/java/com/example/hangman/GuessWord/GuessWord.kt
@@ -1,15 +1,19 @@
 package com.example.hangman.GuessWord
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.hangman.R
 import com.example.hangman.databinding.FragmentGuessWordBinding
 
 class GuessWord : Fragment() {
+
+    private lateinit var viewModel: GuessWordViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
@@ -21,6 +25,12 @@ class GuessWord : Fragment() {
 
         val args = GuessWordArgs.fromBundle(arguments!!)
         binding.disguisedWordTextView.text = args.secretWord
+        Toast.makeText(context, args.secretWord, Toast.LENGTH_LONG).show()
+
+//        viewModel.word.observe(this, Observer { changedWord ->
+////            Log.i("EnterWord", "observer on word invoked, word changed")
+////            binding.enterWordText.setText(changedWord)
+////        })
 
         return binding.root
     }
